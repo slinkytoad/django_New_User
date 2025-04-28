cfbfb235e8602043db60ab8d14110b4ab565ece0
Patrick Palmersheim <ppalmersheim@cjmoyna.com>
2025-04-28 10:19:54 -0500

    maybe

--- new_user/new_user/settings.py
+++ new_user/new_user/settings.py
@@ -17,7 +17,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(dotenv_path='../../.env')
+load_dotenv(dotenv_path='../.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
