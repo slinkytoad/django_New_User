f2055a227112e9ee847e5cc5815bd108298252c3
Patrick Palmersheim <ppalmersheim@cjmoyna.com>
2025-04-28 11:15:55 -0500

    added split

--- new_user/new_user/settings.py
+++ new_user/new_user/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS')
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS').split(',')
 
 
 # Application definition
